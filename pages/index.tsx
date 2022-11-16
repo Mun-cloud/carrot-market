db5c27e36778f309c9ab05284a3bf1db2ee669d3
문태호(Mun TaeHo) <mun05170@naver.com>
2022-11-16 23:44:23 +0900

    enter page html

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,7 @@ import { NextPage } from "next";
 import Image from "next/image";
 
 const Home: NextPage = () => {
-  return (
-    <div className="flex flex-col space-y-2 p-5">
-      <details className="select-none ">
-        <summary className="cursor-pointer">What is my fav. food.</summary>
-        <span className="selection:bg-cyan-400">김치</span>
-      </details>
-    </div>
-  );
+  return null;
 };
 
 export default Home;
