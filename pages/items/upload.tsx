05b398e54d5ff9fedd016763698c731cdf883163
문태호(Mun TaeHo) <mun05170@naver.com>
2022-11-29 00:08:30 +0900

    커뮤니티 페이지

--- pages/items/upload.tsx
+++ pages/items/upload.tsx
@@ -1,47 +1,64 @@
-export default function Upload() {
-  return (
-    <div>
-      <div>
-        <div>
-          <label>
-            <svg
-              className="h-12 w-12"
-              stroke="currentColor"
-              fill="none"
-              viewBox="0 0 48 48"
-              aria-hidden="true"
-            >
-              <path
-                d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                strokeWidth={2}
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
+import { NextPage } from "next";
 
-            <input type="file" />
-          </label>
+const Upload: NextPage = () => {
+  return (
+    <div className="px-4 py-16">
+      <div>
+        <label className="w-full flex items-center justify-center border-2 border-dashed border-gray-300 py-6 rounded-md text-gray-600 hover:text-orange-500 hover:border-orange-500 cursor-pointer">
+          <svg
+            className="h-12 w-12"
+            stroke="currentColor"
+            fill="none"
+            viewBox="0 0 48 48"
+            aria-hidden="true"
+          >
+            <path
+              d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+              strokeWidth={2}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+
+          <input className="hidden" type="file" />
+        </label>
+      </div>
+      <div className="my-5">
+        <label
+          htmlFor="price"
+          className="mb-1 block text-sm font-medium text-gray-700"
+        >
+          Price
+        </label>
+        <div className="rounded-md relative shadow-sm flex items-center">
+          <div className="absolute left-0 pl-3 pointer-events-none flex items-center justify-center">
+            <span className="text-gray-500 text-sm">$</span>
+          </div>
+          <input
+            id="price"
+            type="text"
+            placeholder="0.00"
+            className="appearance-none w-full px-3 py-2 pl-7 border border-gray-300 rounded-md rounded-l-none shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-400 focus:border-orange-500"
+          />
+          <div className="absolute right-0 pointer-events-none pr-3 flex items-center">
+            <span className="text-gray-500">USD</span>
+          </div>
         </div>
       </div>
       <div>
-        <label>Price</label>
-        <div>
-          <div>
-            <span>$</span>
-          </div>
-          <input type="text" placeholder="0.00" />
-          <div>
-            <span>USD</span>
-          </div>
-        </div>
+        <label className="mb-1 block text-sm font-medium text-gray-700">
+          Description
+        </label>
+        <textarea
+          rows={4}
+          className="mt-1 shadow-sm w-full focus:ring-2 rounded-md border-gray-300 focus:border-orange-500"
+        />
       </div>
-      <div>
-        <label>Description</label>
-        <div>
-          <textarea rows={4} />
-        </div>
-      </div>
-      <button>Upload product</button>
+      <button className="mt-5 bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none w-full">
+        Upload product
+      </button>
     </div>
   );
-}
+};
+
+export default Upload;
