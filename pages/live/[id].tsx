dc3f2a76379a2f2d31d890568565092d5ca6bf54
문태호(Mun TaeHo) <mun05170@naver.com>
2022-12-05 22:52:34 +0900

    컴포넌트 리팩토링

--- pages/live/[id].tsx
+++ pages/live/[id].tsx
@@ -1,89 +1,49 @@
 import { NextPage } from "next";
+import Layout from "../../components/layout";
+import Message from "../../components/message";
 
 const LiveDetail: NextPage = () => {
   return (
-    <div className="py-10 px-4 space-y-4">
-      <div className="w-full rounded-md shadow-sm bg-slate-300 aspect-video" />
-      <h3 className="text-gray-800 font-semibold text-2xl mt-2">
-        {"Let's trey potatos"}
-      </h3>
-
-      <div className="mt-10 pb-16 h-[50vh] overflow-y-scroll px-4 space-y-4">
-        <div className="flex items-start gap-2">
-          <div className="w-8 h-8 rounded-full bg-slate-400" />
-          <div className="w-1/2 text-sm border-gray-300 text-gray-700 border p-2 rounded-md">
-            <p>Hi how much are you selling them for?</p>
-          </div>
+    <Layout canGoBack>
+      <div className="py-10 px-4  space-y-4">
+        <div className="w-full rounded-md shadow-sm bg-slate-300 aspect-video" />
+        <div className="mt-5">
+          <h1 className="text-3xl font-bold text-gray-900">Galaxy S50</h1>
+          <span className="text-2xl block mt-3 text-gray-900">$140</span>
+          <p className=" my-6 text-gray-700">
+            My money&apos;s in that office, right? If she start giving me some
+            bullshit about it ain&apos;t there, and we got to go someplace else
+            and get it, I&apos;m gonna shoot you in the head then and there.
+            Then I&apos;m gonna shoot that bitch in the kneecaps, find out where
+            my goddamn money is. She gonna tell me too. Hey, look at me when
+            I&apos;m talking to you, motherfucker. You listen: we go in there,
+            and that ni**a Winston or anybody else is in there, you the first
+            motherfucker to get shot. You understand?
+          </p>
         </div>
-        <div className="flex flex-row-reverse items-start gap-2">
-          <div className="w-8 h-8 rounded-full bg-slate-400" />
-          <div className="w-1/2 text-sm border-gray-300 text-gray-700 border p-2 rounded-md">
-            <p>I want ￦20,000</p>
+        <div>
+          <h2 className="text-2xl font-bold text-gray-900">Live Chat</h2>
+          <div className="py-10 pb-16 h-[50vh] overflow-y-scroll  px-4 space-y-4">
+            <Message message="Hi how much are you selling them for?" />
+            <Message message="I want ￦20,000" reversed />
+            <Message message="미쳤어" />
           </div>
-        </div>
-        <div className="flex items-start gap-2">
-          <div className="w-8 h-8 rounded-full bg-slate-400" />
-          <div className="w-1/2 text-sm border-gray-300 text-gray-700 border p-2 rounded-md">
-            <p>Hi how much are you selling them for?</p>
-          </div>
-        </div>
-        <div className="flex flex-row-reverse items-start gap-2">
-          <div className="w-8 h-8 rounded-full bg-slate-400" />
-          <div className="w-1/2 text-sm border-gray-300 text-gray-700 border p-2 rounded-md">
-            <p>I want ￦20,000</p>
-          </div>
-        </div>
-        <div className="flex items-start gap-2">
-          <div className="w-8 h-8 rounded-full bg-slate-400" />
-          <div className="w-1/2 text-sm border-gray-300 text-gray-700 border p-2 rounded-md">
-            <p>Hi how much are you selling them for?</p>
-          </div>
-        </div>
-        <div className="flex flex-row-reverse items-start gap-2">
-          <div className="w-8 h-8 rounded-full bg-slate-400" />
-          <div className="w-1/2 text-sm border-gray-300 text-gray-700 border p-2 rounded-md">
-            <p>I want ￦20,000</p>
-          </div>
-        </div>
-        <div className="flex items-start gap-2">
-          <div className="w-8 h-8 rounded-full bg-slate-400" />
-          <div className="w-1/2 text-sm border-gray-300 text-gray-700 border p-2 rounded-md">
-            <p>Hi how much are you selling them for?</p>
-          </div>
-        </div>
-        <div className="flex flex-row-reverse items-start gap-2">
-          <div className="w-8 h-8 rounded-full bg-slate-400" />
-          <div className="w-1/2 text-sm border-gray-300 text-gray-700 border p-2 rounded-md">
-            <p>I want ￦20,000</p>
-          </div>
-        </div>
-        <div className="flex items-start gap-2">
-          <div className="w-8 h-8 rounded-full bg-slate-400" />
-          <div className="w-1/2 text-sm border-gray-300 text-gray-700 border p-2 rounded-md">
-            <p>Hi how much are you selling them for?</p>
-          </div>
-        </div>
-        <div className="flex flex-row-reverse items-start gap-2">
-          <div className="w-8 h-8 rounded-full bg-slate-400" />
-          <div className="w-1/2 text-sm border-gray-300 text-gray-700 border p-2 rounded-md">
-            <p>I want ￦20,000</p>
+          <div className="fixed py-2 bg-white  bottom-0 inset-x-0">
+            <div className="flex relative max-w-md items-center  w-full mx-auto">
+              <input
+                type="text"
+                className="shadow-sm rounded-full w-full border-gray-300 focus:ring-orange-500 focus:outline-none pr-12 focus:border-orange-500"
+              />
+              <div className="absolute inset-y-0 flex py-1.5 pr-1.5 right-0">
+                <button className="flex focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 items-center bg-orange-500 rounded-full px-3 hover:bg-orange-600 text-sm text-white">
+                  &rarr;
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-      <div className="fixed bottom-0 py-2 w-full mx-auto max-w-md inset-x-0">
-        <div className="flex items-center relative">
-          <input
-            type="text"
-            className="shadow-sm rounded-full w-full pr-12 border-gray-300 focus:ring-orange-500 focus:outline-none focus:border-orange-500"
-          />
-          <div className="absolute inset-y-0 flex py-1.5 pr-1.5 right-0">
-            <button className="flex items-center bg-orange-500 rounded-full px-3 text-sm text-white hover:bg-orange-600 focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none">
-              &rarr;
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
+    </Layout>
   );
 };
 
