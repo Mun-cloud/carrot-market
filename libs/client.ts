f39f8c84161a56d97017dd1a65e41f1ef4c84be9
문태호(Mun TaeHo) <mun05170@naver.com>
2022-12-14 21:05:52 +0900

    backend api

--- libs/client.ts
+++ libs/client.ts
@@ -1,10 +1,3 @@
 import { PrismaClient } from "@prisma/client";
 
-const client = new PrismaClient();
-
-client.user.create({
-  data: {
-    email: "Hi",
-    name: "태태호",
-  },
-});
+export default new PrismaClient();
