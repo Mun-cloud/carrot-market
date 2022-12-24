865821e1adf37865bfd34e17919bef79115d4bfa
문태호(Mun TaeHo) <mun05170@naver.com>
2022-12-25 00:45:47 +0900

    confirm token find

--- pages/api/user/confirm.tsx
+++ pages/api/user/confirm.tsx
@@ -1,14 +1,28 @@
 import client from "@libs/server/client";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
+import { withIronSessionApiRoute } from "iron-session/next";
 
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
   const { token } = req.body;
-  console.log(token);
+  const exists = await client.token.findUnique({
+    where: {
+      payload: token,
+    },
+    include: { user: true },
+  });
+  if (!exists) res.status(404).end();
+  req.session.user = {
+    id: exists?.userId,
+  };
+  await req.session.save();
   return res.status(200).end();
 }
 
-export default withHandler("POST", handler);
+export default withIronSessionApiRoute(withHandler("POST", handler), {
+  cookieName: "carrotsession",
+  password: "fjoij1okj41o5j6kj2;lk45j6m237lkj73lkj7;3lkj7ndsfg",
+});
