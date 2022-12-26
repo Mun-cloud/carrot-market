10f528abe4a538ee880b3bdef3eb474796333dca
문태호(Mun TaeHo) <mun05170@naver.com>
2022-12-27 06:36:33 +0900

    session code cleaning

--- pages/api/user/confirm.tsx
+++ pages/api/user/confirm.tsx
@@ -1,28 +1,33 @@
 import client from "@libs/server/client";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
-import { withIronSessionApiRoute } from "iron-session/next";
+import { withApiSession } from "@libs/server/withSession";
+
+declare module "iron-session" {
+  interface IronSessionData {
+    user?: {
+      id: number;
+    };
+  }
+}
 
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
   const { token } = req.body;
-  const exists = await client.token.findUnique({
+  const foundToken = await client.token.findUnique({
     where: {
       payload: token,
     },
-    include: { user: true },
   });
-  if (!exists) res.status(404).end();
+  if (!foundToken) return res.status(404).end();
   req.session.user = {
-    id: exists?.userId,
+    id: foundToken.userId,
   };
   await req.session.save();
-  return res.status(200).end();
+  await client.token.deleteMany({ where: { userId: foundToken.userId } });
+  res.json({ ok: true });
 }
 
-export default withIronSessionApiRoute(withHandler("POST", handler), {
-  cookieName: "carrotsession",
-  password: "fjoij1okj41o5j6kj2;lk45j6m237lkj73lkj7;3lkj7ndsfg",
-});
+export default withApiSession(withHandler("POST", handler));
