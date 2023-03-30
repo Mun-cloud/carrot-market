f8e0ba1146af31c656d1b8854eb5cc5f32e1466b
Taeho <mun05170@naver.com>
2023-03-30 17:18:07 +0900

    타입 위치 변경

--- libs/server/withSession.ts
+++ libs/server/withSession.ts
@@ -1,5 +1,12 @@
 import { withIronSessionApiRoute } from "iron-session/next";
 
+declare module "iron-session" {
+  interface IronSessionData {
+    user?: {
+      id: number;
+    };
+  }
+}
 const cookieOptions = {
   cookieName: "carrotsession",
   password: process.env.COOKIE_PASSWORD!,
--- pages/api/users/confirm.ts
+++ pages/api/users/confirm.ts
@@ -3,14 +3,6 @@ import withHandler, { ResponseType } from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 import { withApiSession } from "@libs/server/withSession";
 
-declare module "iron-session" {
-  interface IronSessionData {
-    user?: {
-      id: number;
-    };
-  }
-}
-
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
